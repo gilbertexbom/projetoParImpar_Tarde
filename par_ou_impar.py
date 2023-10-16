89743b48df955918e672a5adb9e3f7ae0a9555b2
Instrutor <gilberto.braga0021@gmail.com>
2023-10-16 14:28:49 -0300

    - Commit inicial do tio.

---
+++ par_ou_impar.py
@@ -0,0 +1,13 @@
+# Projeto Par ou Impar
+
+# Apresentação
+print('\n\t\t\t -- Verificador de Número \n\n')
+
+#Entradas
+num = int(input('Informe um número: '))
+
+# Processamento e Saída
+if (num % 2) == 0:
+    print(f'\n{num} é par!')
+else:
+    print(f'\n{num} é impar!')
